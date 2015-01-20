b2c54e7d3b2660d2c021195a0cbb4cf3914468f6
nicolás <nicolas@darkcookies.de>
2015-01-20 00:10:15 -0800

    <table>

--- index.php
+++ index.php
@@ -5,17 +5,15 @@
     <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
   </head>
   <body>
-<script>
-  jQuery.noConflict();
-  jQuery(document).ready(function(){
-         jQuery("#refresh").load("http://nicolas.hello.darkcookies.de/test/refresh.php");
-         var refreshId = setInterval(function() {
-                 jQuery("#refresh").load("http://nicolas.hello.darkcookies.de/test/refresh.php");
-         }, 1000);
-  });
-</script>
-
-<div id="refresh"></div>
-
-</body>
+  <script>
+    jQuery.noConflict();
+    jQuery(document).ready(function(){
+           jQuery("#refresh").load("http://nicolas.hello.darkcookies.de/test/refresh.php");
+           var refreshId = setInterval(function() {
+                   jQuery("#refresh").load("http://nicolas.hello.darkcookies.de/test/refresh.php");
+           }, 1000);
+    });
+  </script>
+  <div id="refresh"></div>
+  </body>
 </html>
--- refresh.php
+++ refresh.php
@@ -1,4 +1,3 @@
-
 <?php
 $vonseite = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mangorausch&api_key=b3f34d8652bf87d8d1dcbfa5c53d245d";
 $response = @simplexml_load_file($vonseite) or die ("Fehler!");
@@ -7,10 +6,21 @@ echo "<table>";
 
 foreach ($response->recenttracks->track as $tracks) {
 
+	echo "<tr>";
+	echo "<td>";
+
+	$img = $tracks->image[0];
+	echo '<img src="'.$img.'" />';
+
+	echo "</td>";
+	echo "<td>";
     if ($tracks->name)   	echo $tracks->name . "<br>";
     if ($tracks->artist)    echo $tracks->artist . "<br>";
     if ($tracks->mbid)      echo $tracks->mbid;
-
+    echo "</td>";
+    echo "</tr>";
 echo "<br><br>";
 }
+
+echo "</table>";
 ?>
